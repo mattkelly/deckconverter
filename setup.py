267b64e1363c17b3a2650714a36f78202cbd9c79
Norman Joyner <norman.joyner@gmail.com>
2014-02-15 15:16:41 -0500

    Fix typos

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import sys
-from deckbuilder import __version__
+from deckconverter import __version__
 
 try:
     from setuptools import setup
@@ -17,16 +17,16 @@ except ImportError:
 requirements = open('requirements/base.txt').readlines()
 
 setup(
-    name='deckbuilder',
+    name='deckconverter',
     version=__version__,
     author='Matthew Kelly, Norman Joyner',
     author_email='matthew.joseph.kelly@gmail.com, norman.joyner@gmail.com',
-    packages=['deckbuilder'],
-    scripts=['bin/deckbuilder'],
-    url='http://github.com/mattkelly/deckbuilder',
+    packages=['deckconverter'],
+    scripts=['bin/deckconverter'],
+    url='http://github.com/mattkelly/deckconverter',
     license='LICENSE',
     classifiers=[],
     description='A utility for converting between card deck formats',
-    long_description=open('README.md').read()
+    long_description=open('README.md').read(),
     install_requires=requirements
 )
