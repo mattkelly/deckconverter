a4487843c8eb414672daec22b3c9619b82ccfc85
Norman Joyner <norman.joyner@gmail.com>
2014-02-17 01:26:43 -0500

    Add simple add_card test which confirms set_name and mvid are fetched properly

---
+++ deckconverter/tests/test_card.py
@@ -0,0 +1,12 @@
+import unittest 
+from deckconverter.card import Card
+
+class TestCard(unittest.TestCase):
+
+    # setUp method
+    def setUp(self):
+        self.card = Card('Mountain')
+
+    def test_add_card(self):
+        self.assertTrue(self.card.get_mvid() == "207936")
+        self.assertTrue(self.card.get_set() == "Premium Deck Series: Slivers")
