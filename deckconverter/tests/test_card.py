dc8bbcb955930aa6a2861063c658940471143c96
Norman Joyner <norman.joyner@gmail.com>
2014-02-17 18:29:38 -0500

    implement basic function testing

--- deckconverter/tests/test_card.py
+++ deckconverter/tests/test_card.py
@@ -7,10 +7,20 @@ class TestCard(unittest.TestCase):
     def setUp(self):
         self.card = Card('Mountain')
 
-    def test_add_card(self):
-        self.assertTrue(self.card.get_mvid() == "207936")
-        self.assertTrue(self.card.get_set() == "Premium Deck Series: Slivers (Land)")
+    def test_get_name(self):
+        self.assertTrue(self.card.get_name() == 'Mountain')
 
-    def test_add_card_cache(self):
-        self.assertTrue(self.card.get_mvid() == "207936")
-        self.assertTrue(self.card.get_set() == "Premium Deck Series: Slivers (Land)")
+    def test_get_set(self):
+        self.assertTrue(self.card.get_set() == 'Premium Deck Series: Slivers (Land)')
+
+    def test_get_mvid(self):
+        self.assertTrue(self.card.get_mvid() == '207936')
+
+    def test_equality(self):
+        card = Card('Mountain')
+        self.assertTrue(self.card.__eq__(card))
+        card = Card('Island')
+        self.assertFalse(self.card.__eq__(card))
+
+    def test_repr(self):
+        self.assertTrue(self.card.__repr__() == 'Mountain from Premium Deck Series: Slivers (Land) (mvid = 207936)')
