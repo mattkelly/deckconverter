771c0aaf7aa42cb5cf5d1b8f96307ef2cebaa3ba
Norman Joyner <norman.joyner@gmail.com>
2014-02-17 09:55:20 -0500

    Refactor to grab all details from a single request. Improve caching

--- deckconverter/request.py
+++ deckconverter/request.py
@@ -3,70 +3,76 @@ import requests
 
 cache = {}
 
-def get_set_name(name, mvid = ''):
-    if mvid == '':
-        mvid = __get_mvid(name)
-
-    set_name = __serve_from_cache(name, '', mvid)
-    if set_name == '':
-        set_name = __get_set_name(mvid)
-        __add_to_cache(name, set_name, mvid)
-        return set_name
+def get_set_name(name, mvid = None):
+    details = __serve_from_cache(name, None, mvid)
+    if details == None:
+        details = __get_card_details(name, None, mvid)
+        __add_to_cache(name, details, mvid == None)
+        return details['set_name']
     else:
-        return set_name
+        return details['set_name']
 
-def get_mvid(name, set_name = ''):
-    if set_name == '':
-        mvid = __get_mvid(name)
+def get_mvid(name, set_name = None):
+    details = __serve_from_cache(name, set_name)
+    if details == None:
+        details = __get_card_details(name, set_name)
+        __add_to_cache(name, details, set_name == None)
+        return details['mvid']
     else:
-        mvid = __serve_from_cache(name, set_name)
-        if mvid == '':
-            return __get_mvid(name)
-        else:
-            return mvid
+        return details['mvid']
 
-def __get_mvid(name):
-    html = __http_connection('http://gatherer.wizards.com/Pages/Card/Details.aspx?name=' + name)
-    if html != '':
-        soup = BeautifulSoup(html)
-        mvid = soup.find(id='ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_otherSetsValue').find_all('a')[-1].get("href").split("=")[-1]
-        return mvid
-    else:
-        return html
+def __get_card_details(name, set_name = None, mvid = None):
+    response = __http_connection('http://gatherer.wizards.com/Pages/Card/Details.aspx?name=' + name)
+    if response['code'] == 200 and response['body'] != '':
+        soup = BeautifulSoup(response['body'])
 
-def __get_set_name(mvid):
-    html = __http_connection('http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=' + mvid)
-    if html != '':
-        soup = BeautifulSoup(html)
-        set_name = soup.find(id="ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_currentSetSymbol").find_all('a')[-1].get_text()
-        return set_name
+        if mvid == None and set_name == None:
+            mvid = soup.find(id='ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_otherSetsValue').find_all('a')[-1].get('href').split('=')[-1]
+        elif mvid == None and set_name != None:
+            mvid = soup.find(title=set_name).parent.get('href').split('=')[-1]
+    
+        if set_name == None:
+            set_name = soup.find(href='Details.aspx?multiverseid=' + mvid).find('img').get('title')
+
+        return {
+            'set_name': set_name,
+            'mvid': mvid
+        }
     else:
-        return html
+        return {
+            'set_name': set_name,
+            'mvid': mvid
+        }
 
 def __http_connection(url):
     response = requests.get(url)
-    if response.status_code == 200:
-        return response.text
-    else:
-        return ''
+    return {
+        'code': response.status_code,
+        'body': response.text
+    }
 
-def __serve_from_cache(name, set_name = '', mvid = ''):
+def __serve_from_cache(name, set_name = None, mvid = None):
     if name in cache:
         if set_name in cache[name]:
             return cache[name][set_name]
         elif mvid in cache[name]:
             return cache[name][mvid]
+        elif '__latest' in cache[name]:
+            return cache[name]['__latest']
         else:
-            return ''
+            return None
     else:
-        return ''
+        return None
 
-def __add_to_cache(name, set_name, mvid):
+def __add_to_cache(name, details, latest):
     if name not in cache:
         cache[name] = {}
 
-    if set_name not in cache[name]:
-        cache[name][set_name] = mvid
+    if details['set_name'] not in cache[name]:
+        cache[name][details['set_name']] = details
 
-    if mvid not in cache[name]:
-        cache[name][mvid] = set_name
+    if details['mvid'] not in cache[name]:
+        cache[name][details['mvid']] = details
+
+    if latest:
+        cache[name]['__latest'] = details
