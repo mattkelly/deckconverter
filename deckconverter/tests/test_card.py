3d5150903b48cd1aa69ebe8a020c52462d304287
Norman Joyner <norman.joyner@gmail.com>
2014-02-17 09:54:38 -0500

    Add cache test

--- deckconverter/tests/test_card.py
+++ deckconverter/tests/test_card.py
@@ -9,4 +9,8 @@ class TestCard(unittest.TestCase):
 
     def test_add_card(self):
         self.assertTrue(self.card.get_mvid() == "207936")
-        self.assertTrue(self.card.get_set() == "Premium Deck Series: Slivers")
+        self.assertTrue(self.card.get_set() == "Premium Deck Series: Slivers (Land)")
+
+    def test_add_card_cache(self):
+        self.assertTrue(self.card.get_mvid() == "207936")
+        self.assertTrue(self.card.get_set() == "Premium Deck Series: Slivers (Land)")
